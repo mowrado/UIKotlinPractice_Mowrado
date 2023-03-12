0afe9cf445580dd43304894047d9a5cc720d8131
KHS <hyoseo118@gmail.com>
2023-03-12 19:41:25 +0900

    UI + Kotilin 화면 구조 설정

--- app/src/main/java/com/curlyloops/uikotlinpractice_mowrado/MainActivity.kt
+++ app/src/main/java/com/curlyloops/uikotlinpractice_mowrado/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.curlyloops.uikotlinpractice_mowrado
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        확인버튼이 눌리면? 이벤트 달아주자
+
+        okBtn.setClickListener{
+
+            val inputContent = contentEdt.text.toString()
+
+            resultTxt.text = inputContent
+
+        }
+
+
     }
 }
